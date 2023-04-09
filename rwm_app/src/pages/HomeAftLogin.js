135255bcabb57949eec6b8c876d6f85054d0ec2d
Chris Chan <chrischanwz@gmail.com>
2023-04-09 21:50:23 +0800

    pulled, minor update to hm page, mainly to test

--- rwm_app/src/pages/HomeAftLogin.js
+++ rwm_app/src/pages/HomeAftLogin.js
@@ -17,7 +17,7 @@ const responsive = {
     items: 5,
   },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 1500, min: 1024 },
     items: 3,
   },
   tablet: {
@@ -32,19 +32,45 @@ const responsive = {
 
 export default function HomeAftLogin() {
   return (
-    <Carousel responsive={responsive}>
-      <div className="card">
-        <img
-          src="https://images.unsplash.com/photo-1548345680-f5475ea5df84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80"
-          alt="eventLocation"
-        />{" "}
-        <h2>Jonny's Evening Run</h2>
-        <p> Time: 7pm etcetc </p>
-        <Button> Join Event</Button>
+    <>
+      <div style={{ padding: "15px" }}>
+        <p
+          style={{
+            fontFamily: "georgia,garamond,serif",
+            fontSize: "22px",
+            fontStyle: "italic",
+            fontWeight: "bolder",
+          }}
+        >
+          Find Events near you:{" "}
+        </p>
       </div>
-      <div>Item 2</div>
-      <div>Item 3</div>
-      <div>Item 4</div>
-    </Carousel>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div className="carousel" style={{ display: "block", width: "1000px" }}>
+          <Carousel responsive={responsive}>
+            <div className="card" style={{ padding: "20px" }}>
+              <img
+                src="https://images.unsplash.com/photo-1548345680-f5475ea5df84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80"
+                alt="eventLocation"
+              />{" "}
+              <h2>Jonny's Evening Run</h2>
+              <p> Time: 7pm etcetc </p>
+              <Button> Join Event</Button>
+            </div>
+            <div className="card2" style={{ padding: "20px" }}>
+              <img
+                src="https://images.unsplash.com/photo-1548345680-f5475ea5df84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80"
+                alt="eventLocation"
+              />{" "}
+              <h2>Timmy's Evening Run</h2>
+              <p> Time: 1pm etcetc </p>
+              <Button> Join Event</Button>
+            </div>
+            <div>Item 3</div>
+            <div>Item 4</div>
+          </Carousel>
+        </div>
+      </div>
+    </>
   );
 }
