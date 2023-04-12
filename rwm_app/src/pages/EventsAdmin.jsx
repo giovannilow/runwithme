17774e94c18ede574f23ac7605a2847bebc2b79f
Eden Yew <eden_yew@u.nus.edu>
2023-04-13 04:03:57 +0800

    adjustment to eventsAdmin, BarChart and Dashboard data

--- rwm_app/src/pages/EventsAdmin.jsx
+++ rwm_app/src/pages/EventsAdmin.jsx
@@ -1,11 +1,74 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import {
+  getDocs,
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  firestore,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import app from "@/pages/firebase";
 import { FaShoppingBag } from "react-icons/fa";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsThreeDotsVertical, BsTrash, BsPencilSquare } from "react-icons/bs";
 import { mockDataEvents } from "@/data/mockdata.js";
-import { MdEvent } from "react-icons/md";
+import { MdEvent, MdEdit, MdDelete } from "react-icons/md";
 import { WrapItem, Avatar } from "@chakra-ui/react";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Swal from "sweetalert2";
 
 const Events = () => {
+  const db = getFirestore(app);
+  const [events, setEvents] = useState([]);
+  const [totalEvents, setTotalEvents] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const deleteEvent = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.value) {
+        deleteApi(id);
+      }
+    });
+  };
+
+  const deleteApi = async (id, fetchData) => {
+    const eventDoc = doc(db, "events", id);
+    await deleteDoc(eventDoc);
+    Swal.fire("Deleted!", "Your event has been deleted.", "success");
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "events"));
+        const eventsArray = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setEvents(eventsArray);
+        setLoading(false);
+        setTotalEvents(eventsArray.length); // Set the length of eventsArray as totalEvents
+      } catch (error) {
+        console.error("Error fetching events:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <div className="flex justify-between px-4 pt-4">
@@ -14,16 +77,18 @@ const Events = () => {
       <div className="p-4">
         <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
           <div className="my-3 p-2 grid md:grid-cols-5 sm:grid-cols-4 grid-cols-3 items-center justify-between cursor-pointer">
-            <span className="text-left pl-10">Title / Date</span>
-            <span className="sm:text-left text-right ml-10">Time</span>
-            <span className="hidden md:grid">Distance</span>
-            <span className="hidden sm:grid">Location</span>
-            <span className="hidden sm:grid">Details</span>
+            <span className="text-left pl-10">Title / Date & Time</span>
+            <span className="sm:text-left text-right ml-10">
+              Pace (mins/km)
+            </span>
+            <span className="hidden md:grid">Distance (km)</span>
+            <span className="hidden sm:grid">Start Location</span>
+            <span className="hidden sm:grid">Recurrence</span>
           </div>
           <ul>
-            {mockDataEvents.map((events) => (
+            {events.map((data) => (
               <li
-                key={events.id}
+                key={data.id}
                 className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-5 sm:grid-cols-4 grid-cols-3 items-center justify-between cursor-pointer"
               >
                 <div className="flex">
@@ -32,35 +97,47 @@ const Events = () => {
                   </div>
                   <div className="pl-4">
                     <p className="text-gray-800 font-bold whitespace-nowrap">
-                      {events.title}
+                      {data.title}
                     </p>
-                    <p className="text-gray-800 text-sm">{events.Date}</p>
+                    <p className="text-gray-800 text-sm">{data.date}</p>
                   </div>
                 </div>
                 <p className="text-gray-600 sm:text-left text-right">
-                  <span className="ml-10">{events.Time}</span>
+                  <span className="ml-10">{data.pace}</span>
                 </p>
-                <p className="hidden md:flex">{events.Distance}</p>
+                <p className="hidden md:flex">{data.distance}</p>
                 <div className="sm:flex hidden justify-between items-center">
-                  <p>{events.Location}</p>
+                  <p>{data.startLocation}</p>
                 </div>
                 <div className="sm:flex hidden justify-between items-center">
                   <p
                     className={
-                      events.Details == "Competitive Run"
+                      data.recurrence === "one-off"
                         ? "bg-green-200 p-2 rounded-lg"
-                        : events.Details == "Team Challenge"
-                        ? "bg-blue-200 p-2 rounded-lg"
-                        : events.Details == "Virtual Run"
-                        ? "bg-yellow-200 p-2 rounded-lg"
-                        : events.Details.includes("Marathon")
-                        ? "bg-purple-200 p-2 rounded-lg"
-                        : "bg-red-200 p-2 rounded-lg"
+                        : data.recurrence === "recurrent"
+                        ? data.recurrenceFrequency === "biweekly"
+                          ? "bg-blue-200 p-2 rounded-lg"
+                          : data.recurrenceFrequency === "monthly"
+                          ? "bg-yellow-200 p-2 rounded-lg"
+                          : data.recurrenceFrequency === "daily"
+                          ? "bg-red-200 p-2 rounded-lg"
+                          : "bg-purple-200 p-2 rounded-lg"
+                        : ""
                     }
                   >
-                    {events.Details}
+                    {data.recurrence === "one-off"
+                      ? "one-off"
+                      : data.recurrence === "recurrent" &&
+                        data.recurrenceFrequency}
                   </p>
-                  <BsThreeDotsVertical />
+                  <div className="flex items-center ml-1">
+                    <MdEdit className="text-gray-500 hover:text-blue-500 cursor-pointer text-2xl" />
+                    <div className="w-4"></div>
+                    <MdDelete
+                      className="text-gray-500 hover:text-red-500 cursor-pointer text-2xl"
+                      onClick={() => deleteEvent(data.id)}
+                    />
+                  </div>
                 </div>
               </li>
             ))}
